7ab30a9ed443cc0fb67716b936fdf672a4182dc9
sarahzull <85895574+sarahzull@users.noreply.github.com>
2021-11-23 22:22:20 +0800

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,89 +1,85 @@
 @extends('layouts.app')
 @section('content')
+
 <div class="row justify-content-center">
-    <div class="col-md-6 ">
+    <div class="col-md-6">
+
         <div class="card mx-4">
             <div class="card-header text-center">
                 <h3 class="pt-2">{{ trans('panel.site_title') }}</h3>
-                {{-- <p class="text-muted" style="color:#BFBFC3 !important;">{{ trans('panel.site_title') }}</p> --}}
             </div>
-                <div class="card-body p-4">
-                    {{-- <span class="h1">
-                        <img src="{{url('/assets/transparent.svg')}}" width="118" height="46" alt="logo">
-                        {{ trans('panel.site_title') }}
-                    </span> --}}
+            <div class="card-body p-4">
 
-                    <p class="text-muted">{{ trans('global.login') }}</p>
+                <form method="POST" action="{{ route('register') }}">
+                    {{ csrf_field() }}
+                    <p class="text-muted">{{ trans('global.register') }}</p>
 
-                    @if(session('message'))
-                        <div class="alert alert-info" role="alert">
-                            {{ session('message') }}
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">
+                                <i class="fa fa-user fa-fw"></i>
+                            </span>
                         </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="input-group mb-3">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text">
-                                    <i class="fa fa-user"></i>
-                                </span>
+                        <input type="text" name="name" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" required autofocus placeholder="{{ trans('global.user_name') }}" autocomplete="off" required>
+                        @if($errors->has('name'))
+                            <div class="invalid-feedback">
+                                {{ $errors->first('name') }}
                             </div>
+                        @endif
+                    </div>
 
-                            <input id="email" name="email" type="text" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"  autofocus placeholder="{{ trans('global.login_email') }}"  autocomplete="off" required>
-
-                            @if($errors->has('email'))
-                                <div class="invalid-feedback">
-                                    {{ $errors->first('email') }}
-                                </div>
-                            @endif
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">
+                                <i class="fa fa-envelope fa-fw"></i>
+                            </span>
                         </div>
-
-                        <div class="input-group mb-3">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="fa fa-lock"></i></span>
+                        <input type="email" name="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" required placeholder="{{ trans('global.login_email') }}" value="{{ old('email', null) }}" autocomplete="off" required>
+                        @if($errors->has('email'))
+                            <div class="invalid-feedback">
+                                {{ $errors->first('email') }}
                             </div>
+                        @endif
+                    </div>
 
-                            <input id="password" name="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder="{{ trans('global.login_password') }}" autocomplete="off" required >
-
-                            @if($errors->has('password'))
-                                <div class="invalid-feedback">
-                                    {{ $errors->first('password') }}
-                                </div>
-                            @endif
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">
+                                <i class="fa fa-lock fa-fw"></i>
+                            </span>
                         </div>
+                        <input type="password" name="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder="{{ trans('global.login_password') }}" required>
+                        @if($errors->has('password'))
+                            <div class="invalid-feedback">
+                                {{ $errors->first('password') }}
+                            </div>
+                        @endif
+                    </div>
 
-                        <div class="input-group mb-4">
-                            <div class="form-check checkbox">
-                                <input class="form-check-input" name="remember" type="checkbox" id="remember" style="vertical-align: middle;" />
-                                <label class="form-check-label" for="remember" style="vertical-align: middle;">
-                                    {{ trans('global.remember_me') }}
-                                </label>
-                            </div>
+                    <div class="input-group mb-4">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">
+                                <i class="fa fa-lock fa-fw"></i>
+                            </span>
                         </div>
+                        <input type="password" name="password_confirmation" class="form-control" placeholder="{{ trans('global.login_password_confirmation') }}"  required>
+                    </div>
 
-                        <div class="row">
-                            <div class="col-6">
-                                <button type="submit" class="btn btn-primary px-4">
-                                    {{trans('global.login') }}
-                                </button>
-                            </div>
-                            <div class="col-6 text-right">
-                                @if(Route::has('password.request'))
-                                    <a class="mb-6 px-0" href="{{ route('password.request') }}">
-                                        {{ trans('global.forgot_password') }}
-                                    </a><br>
-                                @endif
-                                <a class="px-0" href="{{ route('register') }}">
-                                    {{ trans('global.register') }}
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div> 
+                    <button class="btn btn-block btn-primary">
+                        {{ trans('global.register') }}
+                    </button>
+                </form>
+                
+                <div class="mt-3 text-right">
+                <a href="{{ route('login') }}">
+                    {{ trans('global.login_register') }}
+                </a>
+                </div>
+
             </div>
         </div>
+
     </div>
 </div>
+
 @endsection
