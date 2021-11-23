b819409c3cc5d4be86d62c90ff556f38248e4bc8
sarahzull <85895574+sarahzull@users.noreply.github.com>
2021-11-23 22:19:56 +0800

    Update footer.blade.php

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,1 @@
-<div class="fixed-bottom pb-2 text-sm text-center text-muted">{{ trans('panel.footer') }}</div>
+<div class="fixed-bottom pb-2 text-sm text-center text-muted">{{ trans('panel.footer') }}{{ trans('panel.site_title') }}</div>
