34601178f940517f8788ad7450b1ee88a5e01f6e
sarahzull <85895574+sarahzull@users.noreply.github.com>
2021-11-23 19:20:24 +0800

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,8 +41,6 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
-$app->singleton(\Barryvdh\DomPDF\ServiceProvider::class);
-
 /*
 |--------------------------------------------------------------------------
 | Return The Application
